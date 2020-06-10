73efbe7979472b24775cf389c3251534d3a9dc1b
JP <changjunpyo@gmail.com>
2020-06-10 14:01:31 +0900

    segmentation using detectron2

--- labelme/app.py
+++ labelme/app.py
@@ -36,6 +36,15 @@ import torchvision
 import torchvision.transforms as transforms
 import torchvision.models as models
 
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+from detectron2 import model_zoo
+
+import cv2
+import numpy as np
+
 # FIXME
 # - [medium] Set max zoom value to something big enough for FitWidth/Window
 
@@ -441,6 +450,15 @@ class MainWindow(QtWidgets.QMainWindow):
             enabled=True,
         )
 
+        segmentation = action(
+            self.tr('Image Segmentation'),
+            self.segmentation,
+            shortcuts['segmentation'],
+            'objects',
+            self.tr('segment the whole image'),
+            enabled=True,
+        )
+
         # Lavel list context menu.
         labelMenu = QtWidgets.QMenu()
         utils.addActions(labelMenu, (edit, classifyImage, delete))
@@ -451,6 +469,7 @@ class MainWindow(QtWidgets.QMainWindow):
         # Store actions for further handling.
         self.actions = utils.struct(
             classifyImage=classifyImage,
+            segmentation=segmentation,
             saveAuto=saveAuto,
             saveWithImageData=saveWithImageData,
             changeOutputDir=changeOutputDir,
@@ -501,6 +520,7 @@ class MainWindow(QtWidgets.QMainWindow):
                 undoLastPoint,
                 addPointToEdge,
                 removePoint,
+                segmentation,
             ),
             onLoadActive=(
                 close,
@@ -1775,5 +1795,26 @@ class MainWindow(QtWidgets.QMainWindow):
         _, index = torch.max(out, 1)
         percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
         print(classes[index[0]], percentage[index[0]].item())
+
+        return
+
+    def segmentation(self):
+        img = utils.img_data_to_pil(self.imageData).convert('RGB')
+        img = np.array(img)
+
+        cfg = get_cfg()
+        cfg.merge_from_file("./detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml")
+        cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+        cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_1x.yaml")
+        cfg.MODEL.DEVICE = 'cpu'
+
+        pred = DefaultPredictor(cfg)
+        outputs = pred(img)
+
+        v = Visualizer(img[:,:,::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+        cv2.imshow('Segmented Image', v.get_image())
+
 
         return
